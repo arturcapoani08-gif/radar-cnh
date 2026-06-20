8b6ae21b262f261cda6e5719923d2e378ac4a839
arturcapoani08-gif <arturcapoani08@gmail.com>
2026-06-20 15:18:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════════════
 // Radar CNH — Service Worker v4
 // ═══════════════════════════════════════════════
-const CACHE = 'radar-cnh-v23';
+const CACHE = 'radar-cnh-v24';
 
 const LOCAL_ASSETS = [
   './index.html',
